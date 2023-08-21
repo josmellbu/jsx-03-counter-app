9b8e8fd5fd01b410618f4cdd79c5e31fab72cbf7
ciber-18 <ciber-18@example.com>
2023-08-21 10:45:00 -0500

    fisrt comiit

---
+++ src/CounterApp.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => setCounter(counter + 1);
+  const handleAsd = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(value);
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleAsd}>-1</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired,
+};
---
+++ src/FirstApp.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+      <p>{name}</p>
+    </>
+  );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+FirstApp.defaultProps = {
+  name: 'Alex Josmell',
+  subTitle: 'No hay subtitulo',
+  title: 'No hay titulo',
+};
